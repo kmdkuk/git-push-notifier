5e6513a6113a34fdf60ed55489e5f7d9073e5d3f
kouki <kouworld0123@gmail.com>
2021-06-06 22:10:57 +0900

    .gitを探す機能を追加

--- lib/file/file.go
+++ lib/file/file.go
@@ -1,6 +1,13 @@
 package file
 
-import "fmt"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	"github.com/kmdkuk/git-push-notifier/log"
+)
 
 type File struct {
 	// pushしてないリポジトリを探すルートpath
@@ -14,5 +21,25 @@ func NewFile(root string) *File {
 }
 
 func (f *File) FindGitDir() ([]string, error) {
-	return nil, fmt.Errorf("not find git dir")
+	paths := make([]string, 0)
+	err := filepath.WalkDir(f.root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if _, err := os.Stat(filepath.Join(path, ".git")); !os.IsNotExist(err) {
+			log.Debugf("root %s\n", f.root)
+			log.Debugf("path %s\n", path)
+			rel, err := filepath.Rel(f.root, path)
+			if err != nil {
+				return err
+			}
+			paths = append(paths, filepath.Join("/", rel))
+			return fs.SkipDir
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error %v", err)
+	}
+	return paths, nil
 }
